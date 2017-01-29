20689dd05fe375ad51bed30ed0efabc204927f8e
ashishbhatiya18 <lakhwani.lucky@gmail.com>
2017-01-29 18:06:14 +0530

    Fixing directory names

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "DockerMachine Windows API",
+    name = "dockermachine",
     version = "0.0.1",
     author = "Ashish Bhatiya",
     author_email = "ashishbhatiya18@gmail.com",
