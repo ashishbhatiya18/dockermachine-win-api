b9f0bc293e1515e82a67eaffed49a38f8c04664e
ashishbhatiya18 <lakhwani.lucky@gmail.com>
2017-02-07 04:24:29 +0530

    Added functionality to get list of images available

--- dockermachine/DockerMachine.py
+++ dockermachine/DockerMachine.py
@@ -43,13 +43,22 @@ class machine:
         stdout, stderr = subprocess.Popen('cmd.exe',stdin=subprocess.PIPE,stdout=subprocess.PIPE).\
         communicate(command.getvalue().encode())
         output = stdout.decode('utf-8').split('\n')[10:-1]
-        return [(line.split(" ")[0], line) for line in output]
+        return dict([(line.split(" ")[0], line) for line in output])
+
+    def images(self):
+        command = 'docker images'
+        command = StringIO('\n'.join(self.env+[command]+['exit']))
+        stdout, stderr = subprocess.Popen('cmd.exe',stdin=subprocess.PIPE,stdout=subprocess.PIPE).\
+        communicate(command.getvalue().encode())
+        output = stdout.decode('utf-8').split('\n')[10:-1]
+        return dict([(line.split(" ")[0], line) for line in output])
 
 if __name__=="__main__":
     m = machine()
-    for i in range(10):
-        cid = m.launch('docker run -d --env ADVERTISED_HOST=192.168.99.100 --env ADVERTISED_PORT=9092 spotify/kafka')
-    print(m.containers(all=True))
-    for container in m.containers(all=True):
-        m.stop(container[0])
-    print(m.containers(all=True))
+    # for i in range(5):
+    #     cid = m.launch('docker run -d --env ADVERTISED_HOST=192.168.99.100 --env ADVERTISED_PORT=9092 spotify/kafka')
+    # print(m.containers(all=True).keys())
+    # for container in m.containers(all=True).keys():
+    #     m.stop(container)
+    # print(m.containers(all=True))
+    print(m.images().keys())
