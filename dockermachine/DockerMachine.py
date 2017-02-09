9d78f3e6e46425e2cc9cfbfd46c999a6f4f11388
ashishbhatiya18 <ashishbhatiya18@gmail.com>
2017-02-09 11:39:10 +0530

    Created ip attribute

--- dockermachine/DockerMachine.py
+++ dockermachine/DockerMachine.py
@@ -6,16 +6,17 @@ class Machine:
     name = ''
     env = 'docker-machine env {} | Invoke-Expression'
     shell = 'powershell.exe'
+    ip = ''
 
     def __init__(self, name='hadoop'):
         self.name = name
         self.__start__()
+        self.ip = self.__getip__()
 
     def __start__(self):
-        os.popen('{} {} {}'.format(self.shell, 'docker-machine start',self.name)).read()
-        # subprocess.Popen("docker-machine start "+self.name,stdout=None)
+        os.popen('{} {} {}'.format(self.shell, 'docker-machine start',self.name))
 
-    def getip(self):
+    def __getip__(self):
         return os.popen("docker-machine ip "+self.name).read()[:-1]
 
     def launch(self, container):
@@ -40,15 +41,16 @@ class Machine:
         return [(line.split(',')[0],line.split(',')[1]) for line in output.split('\n') if len(line) > 0]
 
 if __name__=="__main__":
+    from Container import Container
     m = Machine()
     print(m.images())
     kafkaContainer = Container('spotify/kafka','latest')
     kafkaContainer.add_port(2181,2181)
     kafkaContainer.add_port(9092,9092)
-    kafkaContainer.add_env('ADVERTISED_HOST','192.168.99.100')
+    kafkaContainer.add_env('ADVERTISED_HOST',m.ip)
     kafkaContainer.add_env('ADVERTISED_PORT','9092')
     m.launch(kafkaContainer)
     print(m.containers())
     for container in m.containers(all=True).keys():
         print(container)
-        # m.stop(container)
+        m.stop(container)
